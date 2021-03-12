e847901aec5223dbf2071f6750f18e1f37936771
Shaytan-M <73448452+Shaytan-M@users.noreply.github.com>
2021-03-12 22:02:50 +0200

    123

--- Coment list/dist/script.dev.js
+++ Coment list/dist/script.dev.js
@@ -1,42 +1,13 @@
 "use strict";
 
 function onSubmit(event) {
-  event.preventDefault(); // console.log(document.forms[0].childNodes);
-  // console.log(document.forms[0].children);
-  // console.log(document.forms[0].elements);
-  // console.log(document.forms[0].elements[0].value);
-  // console.log(document.forms[0].elements[1].value);
-
+  event.preventDefault();
   console.log(event.target.elements[0].value);
   var d = new Date();
+  var name = document.querySelector('.name');
+  var comment = document.querySelector('.comment_inp');
   var s = document.querySelector('.wrapper');
   s.innerHTML += "<div class=\"block\">\n    <h2 class=\"name_user\">".concat(event.target.elements[0].value, "</h2>\n    <p class=\"time\">").concat(d.toLocaleDateString(), "</p>\n    <p class=\"comment\">").concat(event.target.elements[1].value, "</p>\n   </div> <hr>");
-} // let arrCountry = [
-//     'Ukraine',
-//     'Usa',
-//     'Russia',
-//     'Poland',
-//     'Portugal',
-//     'Peru',
-// ]
-// let arrSearch = [
-// ]
-// let serchStr = '';
-// function textinp(event){
-//     serchStr = event.target.value;
-//     visibleSuList();
-// }
-// function visibleSuList(){
-//     const list = document.querySelector('#some');
-//     if(serchStr){
-//         list.innerHTML = ''
-//         printItem(arrCountry.filter(item => item.includes(serchStr)), list)
-//     }else {
-//         list.innerHTML = '';
-//     }
-// }
-// function printItem(arr, element){
-//     for (const item of arr) {
-//         element.innerHTML += `<div class="item">${item}</div>`
-//     }
-// }
+  name.value = '';
+  comment.value = '';
+}
--- Coment list/script.js
+++ Coment list/script.js
@@ -4,25 +4,24 @@ function onSubmit(event){
    
 
 
-    // console.log(document.forms[0].childNodes);
-    // console.log(document.forms[0].children);
-    // console.log(document.forms[0].elements);
-    // console.log(document.forms[0].elements[0].value);
-    // console.log(document.forms[0].elements[1].value);
-
 
     console.log(event.target.elements[0].value);
 
     let d = new Date();
 
-
-    let s = document.querySelector('.wrapper')
+    let name = document.querySelector('.name');
+    let comment = document.querySelector('.comment_inp')
+    let s = document.querySelector('.wrapper');
 
     s.innerHTML += `<div class="block">
     <h2 class="name_user">${event.target.elements[0].value}</h2>
     <p class="time">${d.toLocaleDateString()}</p>
     <p class="comment">${event.target.elements[1].value}</p>
    </div> <hr>`
+
+
+   name.value = '';
+   comment.value = '';
 }
 
 
@@ -31,56 +30,4 @@ function onSubmit(event){
 
 
 
-
-
-
-
-
-
-
-
-// let arrCountry = [
-//     'Ukraine',
-//     'Usa',
-//     'Russia',
-//     'Poland',
-//     'Portugal',
-//     'Peru',
-// ]
-
-// let arrSearch = [
-
-// ]
-
-
-
-// let serchStr = '';
-
-
-
-// function textinp(event){
-
-//     serchStr = event.target.value;
-
-//     visibleSuList();
-    
-   
-// }
-
-// function visibleSuList(){
-//     const list = document.querySelector('#some');
-//     if(serchStr){
-//         list.innerHTML = ''
-//         printItem(arrCountry.filter(item => item.includes(serchStr)), list)
-//     }else {
-//         list.innerHTML = '';
-//     }
-// }
-
-
-// function printItem(arr, element){
-//     for (const item of arr) {
-//         element.innerHTML += `<div class="item">${item}</div>`
-//     }
-// }
 
