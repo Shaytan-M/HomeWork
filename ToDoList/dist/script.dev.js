72209c2bbf5fb0f955afbde894d028999503bf04
Shaytan-M <73448452+Shaytan-M@users.noreply.github.com>
2021-04-26 18:43:35 +0300

    ToDoList

--- ToDoList/dist/script.dev.js
+++ ToDoList/dist/script.dev.js
@@ -4,28 +4,67 @@ function save(dataInput) {
   dataInput.preventDefault();
 
   if (editable) {
-    putData('https://testinapi.herokuapp.com/todos/', {
-      id: editable.id,
-      title: dataInput.target.enter.value
-    }).then(function (res) {
-      editable = null;
-      sub.value = 'Save';
-      printInfo();
-    });
+    if (currentFile) {
+      postFile(currentFile).then(function (resp) {
+        console.log(resp);
+        fileId = resp[0].id;
+        putData('https://testinapi.herokuapp.com/todos/', {
+          id: editable.id,
+          title: dataInput.target.enter.value,
+          photo: fileId
+        }).then(function (res) {
+          editable = null;
+          sub.value = 'Save';
+          printInfo();
+        });
+        enter.value = '';
+        document.querySelector('.file_img').style['background'] = "url('none')";
+      });
+    } else {
+      putData('https://testinapi.herokuapp.com/todos/', {
+        id: editable.id,
+        title: dataInput.target.enter.value
+      }).then(function (res) {
+        editable = null;
+        sub.value = 'Save';
+        printInfo();
+        enter.value = '';
+      });
+    }
   } else {
-    postTodo('https://testinapi.herokuapp.com/todos', {
-      title: dataInput.target.enter.value
-    }).then(function (response) {
-      printInfo();
-    });
-  }
+    var _fileId;
 
-  enter.value = '';
+    if (currentFile) {
+      postFile(currentFile).then(function (resp) {
+        console.log(resp);
+        _fileId = resp[0].id;
+        postTodo('https://testinapi.herokuapp.com/todos', {
+          title: dataInput.target.enter.value,
+          photo: _fileId
+        }).then(function (response) {
+          printInfo();
+        });
+        enter.value = '';
+        document.querySelector('.file_img').style['background'] = "url('none')";
+      });
+    } else {
+      postTodo('https://testinapi.herokuapp.com/todos', {
+        title: dataInput.target.enter.value
+      }).then(function (response) {
+        printInfo();
+        enter.value = '';
+      });
+    }
+  }
 }
 
+var loader = document.querySelector('.load');
+
 function printInfo() {
+  loader.style.display = 'block';
+  ToDoList.innerHTML = '';
   getData('https://testinapi.herokuapp.com/todos').then(function (data) {
-    ToDoList.innerHTML = '';
+    loader.style.display = 'none';
     var _iteratorNormalCompletion = true;
     var _didIteratorError = false;
     var _iteratorError = undefined;
@@ -33,7 +72,7 @@ function printInfo() {
     try {
       for (var _iterator = data[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
         var item = _step.value;
-        ToDoList.innerHTML += "<div style=\"background-color: ".concat(item.isDone ? 'gray' : '', " ;\" class=\"row\"><form id=").concat(item.id, "><div name=\"left\"> <p class=\"title\" name=\"title\">").concat(item.title, "</p></div> <div><button onclick=\"editInput(event)\">&#9997</button> <button onclick=\"deleteInput(event)\" >&#10006</button> </div></form></div>");
+        ToDoList.innerHTML += "<div style=\"background-color: ".concat(item.isDone ? 'gray' : '', " ;\" class=\"row\"><form id=").concat(item.id, "><img src=\"").concat(item.photo ? 'https://testinapi.herokuapp.com' + item.photo.url : 'http://images2.minutemediacdn.com/image/upload/c_crop,h_1193,w_2121,x_0,y_64/f_auto,q_auto,w_1100/v1565279671/shape/mentalfloss/578211-gettyimages-542930526.jpg', "\" id=\"img\"><div name=\"left\"> <p class=\"title\" name=\"title\">").concat(item.title, "</p></div> <div><button onclick=\"editInput(event)\">&#9997</button> <button onclick=\"deleteInput(event)\" >&#10006</button> </div></form></div>");
       }
     } catch (err) {
       _didIteratorError = true;
@@ -122,8 +161,12 @@ var editable;
 
 function editInput(dataInput) {
   dataInput.preventDefault();
+  console.log();
   var id = dataInput.target.parentNode.parentNode.id;
   getData("https://testinapi.herokuapp.com/todos/".concat(id)).then(function (data) {
+    console.log(data);
+    var labelFile = document.querySelector('.file_img');
+    labelFile.style['background-image'] = "url('".concat(data.photo ? 'https://testinapi.herokuapp.com' + data.photo.url : '', "')");
     enter.value = data.title;
     sub.value = 'Edit';
     editable = data;
@@ -159,4 +202,55 @@ function putData(url, data) {
       }
     }
   });
+}
+
+var apiUrlUpload = 'https://testinapi.herokuapp.com/upload';
+
+function postFile(file) {
+  var formData, resp;
+  return regeneratorRuntime.async(function postFile$(_context4) {
+    while (1) {
+      switch (_context4.prev = _context4.next) {
+        case 0:
+          formData = new FormData();
+          formData.append('files', file);
+          _context4.prev = 2;
+          _context4.next = 5;
+          return regeneratorRuntime.awrap(fetch(apiUrlUpload, {
+            method: 'POST',
+            body: formData
+          }));
+
+        case 5:
+          resp = _context4.sent;
+          _context4.next = 8;
+          return regeneratorRuntime.awrap(resp.json());
+
+        case 8:
+          return _context4.abrupt("return", _context4.sent);
+
+        case 11:
+          _context4.prev = 11;
+          _context4.t0 = _context4["catch"](2);
+          console.log(error);
+
+        case 14:
+        case "end":
+          return _context4.stop();
+      }
+    }
+  }, null, null, [[2, 11]]);
+}
+
+var currentFile;
+var srcFile;
+
+function selectFile(event) {
+  if (event.target.files[0]) {
+    currentFile = event.target.files[0];
+    srcFile = URL.createObjectURL(currentFile);
+    console.log(currentFile);
+    var labelFile = document.querySelector('.file_img');
+    labelFile.style['background-image'] = "url('".concat(srcFile, "')");
+  }
 }
--- ToDoList/script.js
+++ ToDoList/script.js
@@ -1,63 +1,110 @@
-function save(dataInput){
+function save(dataInput) {
     dataInput.preventDefault()
 
 
 
 
-   if(editable){
-    putData('https://testinapi.herokuapp.com/todos/', {id: editable.id, title:dataInput.target.enter.value}).then(res => {
+    if (editable) {
 
-
-        editable = null;
-        sub.value = 'Save';
-
-        printInfo();
-       
+        if (currentFile) {
 
 
 
-    })     
+            postFile(currentFile).then(resp => {
+                console.log(resp);
+                fileId = resp[0].id;
+                putData('https://testinapi.herokuapp.com/todos/', {
+                    id: editable.id,
+                    title: dataInput.target.enter.value,
+                    photo: fileId
+                }).then(res => {
+                    editable = null;
+                    sub.value = 'Save';
+                    printInfo();
 
-   }else{
-    postTodo('https://testinapi.herokuapp.com/todos', { title: dataInput.target.enter.value}).then(response => {
-        printInfo()
-    });
+                })
+                enter.value = '';
+                document.querySelector('.file_img').style['background'] = `url('none')`;
+            });
+        } else {
+            putData('https://testinapi.herokuapp.com/todos/', {
+                id: editable.id,
+                title: dataInput.target.enter.value
+            }).then(res => {
+                editable = null;
+                sub.value = 'Save';
+                printInfo();
+                enter.value = '';
 
 
-   }
+            })
+        }
 
-   enter.value = ''
-}
+    } else {
+
+        let fileId;
+        if (currentFile) {
+
+
+
+            postFile(currentFile).then(resp => {
+                console.log(resp);
+                fileId = resp[0].id;
+                postTodo('https://testinapi.herokuapp.com/todos', {
+                        title: dataInput.target.enter.value,
+                        photo: fileId
+                    })
+                    .then(response => {
+                        printInfo()
+
+                    });
+                enter.value = '';
+                document.querySelector('.file_img').style['background'] = `url('none')`;
+            });
+        } else {
+            postTodo('https://testinapi.herokuapp.com/todos', {
+                title: dataInput.target.enter.value
+            }).then(response => {
+                printInfo()
+                enter.value = '';
+
+            });
+        }
 
 
 
 
-
-
-function printInfo(){
-    
-getData('https://testinapi.herokuapp.com/todos').then(data => {
-
-ToDoList.innerHTML = ''
-
-    for(const item of data){
-        
-
-        ToDoList.innerHTML += `<div style="background-color: ${item.isDone ? 'gray' : ''} ;" class="row"><form id=${item.id}><div name="left"> <p class="title" name="title">${item.title}</p></div> <div><button onclick="editInput(event)">&#9997</button> <button onclick="deleteInput(event)" >&#10006</button> </div></form></div>`
 
 
     }
 
-})
+
+}
+
+
+
+var loader = document.querySelector('.load');
+
+
+
+function printInfo() {
+    loader.style.display = 'block';
+    ToDoList.innerHTML = '';
+    getData('https://testinapi.herokuapp.com/todos').then(data => {
+        loader.style.display = 'none';
+        for (const item of data) {
+            ToDoList.innerHTML += `<div style="background-color: ${item.isDone ? 'gray' : ''} ;" class="row"><form id=${item.id}><img src="${item.photo ? 'https://testinapi.herokuapp.com' + item.photo.url : 'http://images2.minutemediacdn.com/image/upload/c_crop,h_1193,w_2121,x_0,y_64/f_auto,q_auto,w_1100/v1565279671/shape/mentalfloss/578211-gettyimages-542930526.jpg'}" id="img"><div name="left"> <p class="title" name="title">${item.title}</p></div> <div><button onclick="editInput(event)">&#9997</button> <button onclick="deleteInput(event)" >&#10006</button> </div></form></div>`
+        }
+    })
 }
 
 printInfo()
 
 
 
-async function getData(url){
- const resp = await fetch(url);
- return await resp.json();
+async function getData(url) {
+    const resp = await fetch(url);
+    return await resp.json();
 }
 
 async function postTodo(url, data = {}) {
@@ -70,45 +117,51 @@ async function postTodo(url, data = {}) {
     });
 
 
-    
+
 
 }
 
 
 
 
-function deleteInput(dataInput){
+function deleteInput(dataInput) {
     dataInput.preventDefault()
 
 
     let id = dataInput.target.parentNode.parentNode.id;
 
-    fetch(`https://testinapi.herokuapp.com/todos/${id}`, {method: 'DELETE'}).then(data => {
-        
+    fetch(`https://testinapi.herokuapp.com/todos/${id}`, {
+        method: 'DELETE'
+    }).then(data => {
+
 
 
         printInfo()
     })
 
-   
+
 }
 
 let editable;
 
 
-function editInput(dataInput){
-    dataInput.preventDefault()
+function editInput(dataInput) {
+    dataInput.preventDefault();
+
+    console.log();
 
 
     let id = dataInput.target.parentNode.parentNode.id;
 
     getData(`https://testinapi.herokuapp.com/todos/${id}`).then(data => {
 
-    
+        console.log(data);
+        let labelFile = document.querySelector('.file_img');
+        labelFile.style['background-image'] = `url('${data.photo ? 'https://testinapi.herokuapp.com' + data.photo.url : ''}')`;
         enter.value = data.title
         sub.value = 'Edit';
         editable = data;
-       
+
 
 
 
@@ -116,13 +169,13 @@ function editInput(dataInput){
 
 
 
-    
+
 }
 
 
 
 
-async function putData(url, data){
+async function putData(url, data) {
     const response = await fetch(url + data.id, {
         method: 'PUT',
         headers: {
@@ -132,4 +185,51 @@ async function putData(url, data){
     });
 
     return await response.json()
+}
+
+
+
+const apiUrlUpload = 'https://testinapi.herokuapp.com/upload'
+
+
+async function postFile(file) {
+    const formData = new FormData();
+
+    formData.append('files', file);
+
+    try {
+        const resp = await fetch(apiUrlUpload, {
+            method: 'POST',
+            body: formData
+        });
+        return await resp.json()
+    } catch {
+        console.log(error);
+    }
+}
+
+
+let currentFile;
+let srcFile;
+
+function selectFile(event) {
+
+    if (event.target.files[0]) {
+        currentFile = event.target.files[0];
+
+        srcFile = URL.createObjectURL(currentFile)
+
+
+        console.log(currentFile);
+
+        let labelFile = document.querySelector('.file_img');
+        labelFile.style['background-image'] = `url('${srcFile}')`;
+
+
+
+
+
+
+    }
+
 }
